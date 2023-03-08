362f1383677efa98e8195a99eb65aacb8816418a
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2023-03-08 20:36:33 +0900

    230308 - StudentDAO Interface, StudentDAOImple create

---
+++ hyweb/src/main/java/com/hy/student/model/StudentDAO.java
@@ -0,0 +1,7 @@
+package com.hy.student.model;
+
+public interface StudentDAO {
+	
+	public int studentAdd(StudentDTO dto);
+
+}
---
+++ hyweb/src/main/java/com/hy/student/model/StudentDAOImple.java
@@ -0,0 +1,11 @@
+package com.hy.student.model;
+
+public class StudentDAOImple implements StudentDAO {
+
+	@Override
+	public int studentAdd(StudentDTO dto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
